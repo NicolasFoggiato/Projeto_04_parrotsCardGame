4a0859d6b67e154146aff9ebf306094158d8ba33
Nicolas <nicolasfoggiato@gmail.com>
2022-10-24 17:00:15 -0300

    versão finalizada com todos os requisitos completos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numberCards=''
+let numberCards;
 let characters = [
     "bobrossparrot.gif",
     "explodyparrot.gif",
@@ -9,6 +9,64 @@ let characters = [
     "unicornparrot.gif"
 ];
 let cheap =[]
+let plays =0;
+let firstCard
+let secondCard
+let itPlayed = 0
+
+// function hitCounter(){
+//     if (jogadas === qtdeCartas /2 ){
+//        setTimeout(ganhou, 2000)
+//         function ganhou(){alert (`Você ganhou em ${jogou /2} jogadas`)}
+        
+        
+//     }]
+
+
+
+
+// function hitCounter(){
+//     if (plays === numberCards/2){
+//         alert (`'você ganhou em ${itPlayed /2} jogadas'` );
+
+//         const response = prompt('deseja jogar novamente? ')
+//     }
+
+// }
+function upturnedCard(){
+    firstCard.classList.remove("virar")
+    secondCard.classList.remove("virar")
+    firstCard = undefined;
+    secondCard = undefined;
+}
+
+function turnCard(card){
+    itPlayed++
+    card.classList.add('virar')
+    if(firstCard === undefined){
+        firstCard = card
+    }else if(secondCard === undefined){
+        secondCard = card
+    }
+    if(firstCard.innerHTML === secondCard.innerHTML){
+        plays++
+       firstCard =undefined;
+       secondCard = undefined;
+        
+
+        console.log(plays);
+        console.log(numberCards);
+        if (plays === numberCards /2){
+            alert (`'você ganhou em ${itPlayed} jogadas'`);
+
+
+        }
+
+    }else{ setTimeout(upturnedCard, 1000)
+
+    }
+
+}
 
 function pullLetter(){
 
